1f2082dc604096fdb63b2d7fe32645dc70f673ff
Talha Gamga <talhagamga@gmail.com>
2025-10-01 16:01:01 +0300

    Create SoundManager

--- Assets/GAME/Scripts/HitscanBullet.cs
+++ Assets/GAME/Scripts/HitscanBullet.cs
@@ -6,6 +6,7 @@ public class HitscanBullet : MonoBehaviour, IBullet
 {
     public event Action<BulletHitInfo> OnBulletHit;
     [SerializeField] private LayerMask _hitMask;
+    [SerializeField] private SoundData _fireSound;
 
     public void Fire(Vector3 origin, Vector3 direction, float range)
     {
@@ -20,6 +21,10 @@ public class HitscanBullet : MonoBehaviour, IBullet
                 Direction = direction,
                 HitObject = hit.collider.gameObject
             };
+
+            SoundManager.Instance.CreateSoundBuilder()
+                .WithRandomPitch(-0.25f, 0.25f)
+                .Play(_fireSound);
 
             OnBulletHit?.Invoke(hitInfo);
         }
